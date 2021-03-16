f6c4df55ce32dc91bce3f4235404105d614dde93
colinflahive <cflahive@hotmail.com>
2021-03-16 07:53:36 +0000

    Flask App created

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+#   CONFIRGURATION
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
